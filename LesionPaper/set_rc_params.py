481353169a6233682aaa9223280cd22c39ffb649
msafaie <mostafa.safaie@gmail.com>
2019-10-16 13:46:32 +0200

    Copy from Time Paper

---
+++ LesionPaper/set_rc_params.py
@@ -0,0 +1,10 @@
+import matplotlib
+def set_rc_params(**kwarg):
+    matplotlib.rcParams['xtick.major.pad'] = 1
+    matplotlib.rcParams['ytick.major.pad'] = 1
+    matplotlib.rcParams['axes.labelpad']   = 2
+    matplotlib.rcParams['axes.titlepad']   = 3
+    
+    
+    for key,val in kwarg.items():
+        matplotlib.rcParams[key] = val 
