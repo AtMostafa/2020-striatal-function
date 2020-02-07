a18d32dde23174d8bdd4eb2f445ffcab53719bed
msafaie <mostafa.safaie@gmail.com>
2020-02-07 18:07:05 +0100

    Text generated from latex

--- LesionPaper/set_rc_params.py
+++ LesionPaper/set_rc_params.py
@@ -8,6 +8,11 @@ def set_rc_params(dictArg:dict ={}):
     matplotlib.rcParams['axes.labelsize']   = 'small'
     matplotlib.rcParams['xtick.labelsize']   = 'x-small'
     matplotlib.rcParams['ytick.labelsize']   = 'x-small'
+    matplotlib.rcParams['text.usetex'] = True
+    matplotlib.rcParams['pgf.rcfonts'] = False
+    matplotlib.rcParams['pgf.texsystem'] = 'pdflatex'
+    matplotlib.rcParams['pgf.preamble'] = [
+         r"\usepackage[utf8]{inputenc}",r"\usepackage[T1]{fontenc}",r"\usepackage{libertine}"]
 
     
     
